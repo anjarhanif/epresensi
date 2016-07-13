8325fe4b4ec5191bc21c8301c5f25571e72b65e9
yasrul <yasrul@gmail.com>
2016-07-13 16:15:46 +0800

    2016-7-13

---
+++ controllers/ReportController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace app\controllers;
+
+use yii\web\Controller;
+use yii\filters\AccessControl;
+use yii\filters\VerbFilter;
+
+class ReportController extends Controller
+{
+    public function behaviors()
+    {
+        return [
+            'access' => [
+                'class' => AccessControl::className(),
+                'only' => ['logout'],
+                'rules' => [
+                    [
+                        'actions' => ['logout'],
+                        'allow' => true,
+                        'roles' => ['@'],
+                    ],
+                ],
+            ],
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [
+                    'logout' => ['post'],
+                ],
+            ],
+        ];
+    }
+    
+    public function actionIndex()
+    {
+        return $this->render('index');
+    }
+
+}
