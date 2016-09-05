4b3c09c7272d6efcc9125525ae389fa19e89f90a
yasrul <yasrul@gmail.com>
2016-09-05 13:32:16 +0800

    2016-9-5

--- config/web.php
+++ config/web.php
@@ -57,16 +57,16 @@ $config = [
 ];
 
 if (YII_ENV_DEV) {
-    // configuration adjustments for 'dev' environment
-    //$config['bootstrap'][] = 'debug';
-    //$config['modules']['debug'] = [
-    //    'class' => 'yii\debug\Module',
-    //];
+    //configuration adjustments for 'dev' environment
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = [
+        'class' => 'yii\debug\Module',
+    ];
 
-    //$config['bootstrap'][] = 'gii';
-    //$config['modules']['gii'] = [
-    //    'class' => 'yii\gii\Module',
-    //];
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = [
+        'class' => 'yii\gii\Module',
+    ];
 }
 
 return $config;
